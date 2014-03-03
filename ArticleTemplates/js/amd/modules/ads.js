cb0f3f0647412d3dea8fd370893ddad79558c4c9
Sam Morris <mail@sammorrisdesign.co.uk>
2014-03-03 16:52:06 +0000

    Fixing adverts

--- ArticleTemplates/js/amd/modules/ads.js
+++ ArticleTemplates/js/amd/modules/ads.js
@@ -1,127 +1,141 @@
 /*global window,document,console,define */
 define([
-    'modules/$'
+	'modules/$'
 ], function (
-    $
+	$
 ) {
-    'use strict';
+	'use strict';
 
-    var tabletMpuId = 'advert-mpu-content',
-        mobileMpuId = 'advert-mobile-mpu-content',
-        bannerHtmlId = 'advert-banner-content',
+	var tabletMpuId = 'advert-mpu-content',
+		mobileMpuId = 'advert-mobile-mpu-content',
+		bannerHtmlId = 'advert-banner-content',
 
-        modules = {
-            addGoogleTags: function (config) {
-                var googletag = window.googletag = window.googletag || {};
-                googletag.cmd = window.googletag.cmd || [];
+		modules = {
+			addGoogleTags: function (config) {
+				var googletag = window.googletag = window.googletag || {};
+				googletag.cmd = window.googletag.cmd || [];
 
-                var gads = document.createElement('script'),
-                    useSSL = 'https:' === document.location.protocol,
-                    node = document.getElementsByTagName('script')[0];
+				var gads = document.createElement('script'),
+					useSSL = 'https:' === document.location.protocol,
+					node = document.getElementsByTagName('script')[0];
 
-                gads.async = true;
-                gads.type = 'text/javascript';
-                gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
-                node.parentNode.insertBefore(gads, node);
+				gads.async = true;
+				gads.type = 'text/javascript';
+				gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+				node.parentNode.insertBefore(gads, node);
 
-                var windowWidth = window.innerWidth,
-                    bannerWidth = 900,
-                    bannerHeight = 250;
+				var windowWidth = window.innerWidth,
+					bannerWidth = 900,
+					bannerHeight = 250;
 
-                if (windowWidth < 300) {
-                    bannerWidth = 216;
-                    bannerHeight = 36;
-                }
+				if (windowWidth < 300) {
+					bannerWidth = 216;
+					bannerHeight = 36;
+				}
 
-                if (windowWidth < 728 && windowWidth >= 300) {
-                    bannerWidth = 300;
-                    bannerHeight = 50;
-                }
+				if (windowWidth < 728 && windowWidth >= 300) {
+					bannerWidth = 300;
+					bannerHeight = 50;
+				}
 
-                if (windowWidth < 900 && windowWidth >= 728) {
-                    bannerWidth = 728;
-                    bannerHeight = 90;
-                }
+				if (windowWidth < 900 && windowWidth >= 728) {
+					bannerWidth = 728;
+					bannerHeight = 90;
+				}
 
-                googletag.cmd.push(function () {
-                    googletag.defineSlot(config.adsSlot, [[300, 250]], tabletMpuId).addService(googletag.pubads());
-                    googletag.defineSlot(config.adsSlot, [[300, 250]], mobileMpuId).addService(googletag.pubads());
-                    googletag.defineSlot(config.adsSlot, [[bannerWidth, bannerHeight]], bannerHtmlId).addService(googletag.pubads());
-                    googletag.pubads().enableSingleRequest();
-                    googletag.enableServices();
-                });
-            },
+				googletag.cmd.push(function () {
+					googletag.defineSlot(config.adsSlot, [[300, 250]], tabletMpuId).addService(googletag.pubads());
+					googletag.defineSlot(config.adsSlot, [[300, 250]], mobileMpuId).addService(googletag.pubads());
+					googletag.defineSlot(config.adsSlot, [[bannerWidth, bannerHeight]], bannerHtmlId).addService(googletag.pubads());
+					googletag.pubads().enableSingleRequest();
+					googletag.enableServices();
+				});
+			},
 
-            insertAds: function () {
-                var googletag = window.googletag,
-                    windowWidth = window.innerWidth;
+			insertAds: function () {
+				var googletag = window.googletag,
+					windowWidth = window.innerWidth;
+				
+				var counter = 0;
+				
+				$(".article__body > *:nth-child(-n+3)").each(function() {
+					
+					var tagName = $(this)[0].tagName;
+					
+					if (tagName == "P" || tagName == "H2" || tagName == "BLOCKQUOTE") {
+						counter++;
+					} else if (tagName == "FIGURE" && $(this).hasClass("element-video-thumb") || $(this).hasClass("element-video")) {
+						counter++;
+					}
+					
+				});
 
-                if (windowWidth > 450) {
-                    var tabletMpuHtml = "<div id='advert-mpu'>" +
-                                            "<div class='advert-label'>Advertisement " +
-                                                "<div class='icon-container' onclick='javascript:window.location.href=&quot;x-gu://subscribe&quot;'>" +
-                                                    "<div class='touchpoint touchpoint--secondary'>" +
-                                                        "<span class='icon'>&#xe040;</span>" +
-                                                    "</div>" +
-                                                "</div>" +
-                                            "</div>" +
-                                            "<div class='advert-wrapper'>" +
-                                                "<div id=" + tabletMpuId + "></div>" +
-                                            "</div>" +
-                                        "</div>";
+				if (windowWidth > 450 && counter == 3) {
+					var tabletMpuHtml = "<div id='advert-mpu'>" +
+											"<div class='advert-label'>Advertisement " +
+												"<div class='icon-container' onclick='javascript:window.location.href=&quot;x-gu://subscribe&quot;'>" +
+													"<div class='touchpoint touchpoint--secondary'>" +
+														"<span class='icon'>&#xe040;</span>" +
+													"</div>" +
+												"</div>" +
+											"</div>" +
+											"<div class='advert-wrapper'>" +
+												"<div id=" + tabletMpuId + "></div>" +
+											"</div>" +
+										"</div>";
 
-                    $(".article__body > p:nth-of-type(1)").before(tabletMpuHtml);
+					$(".article__body > p:nth-of-type(1)").before(tabletMpuHtml);
 
-                    googletag.cmd.push(function () {
-                        googletag.display(tabletMpuId);
-                    });
+					googletag.cmd.push(function () {
+						googletag.display(tabletMpuId);
+					});
 
-                } else if (windowWidth <= 450) {
-                    var mobileMpuHtml = "<div id='advert-mobile-mpu'>" +
-                                            "<div class='advert-label'>Advertisement " +
-                                                "<div class='icon-container' onclick='javascript:window.location.href=&quot;x-gu://subscribe&quot;'>" +
-                                                    "<div class='touchpoint touchpoint--secondary'>" +
-                                                        "<span class='icon'>&#xe040;</span>" +
-                                                    "</div>" +
-                                                "</div>" +
-                                            "</div>" +
-                                            "<div class='advert-wrapper'>" +
-                                                "<div id=" + mobileMpuId + "></div>" +
-                                            "</div>" +
-                                        "</div>",
+				} else if (windowWidth <= 450) {
+					var mobileMpuHtml = "<div id='advert-mobile-mpu'>" +
+											"<div class='advert-label'>Advertisement " +
+												"<div class='icon-container' onclick='javascript:window.location.href=&quot;x-gu://subscribe&quot;'>" +
+													"<div class='touchpoint touchpoint--secondary'>" +
+														"<span class='icon'>&#xe040;</span>" +
+													"</div>" +
+												"</div>" +
+											"</div>" +
+											"<div class='advert-wrapper'>" +
+												"<div id=" + mobileMpuId + "></div>" +
+											"</div>" +
+										"</div>",
 
-                        bannerHtml = "<div id='advert-banner'>" +
-                                        "<div class='advert-label'>Advertisement </div>" +
-                                        "<div class='advert-wrapper'>" +
-                                            "<div id=" + bannerHtmlId + "></div>" +
-                                        "</div>" +
-                                     "</div>";
+						bannerHtml = "<div id='advert-banner'>" +
+										"<div class='advert-label'>Advertisement </div>" +
+										"<div class='advert-wrapper'>" +
+											"<div id=" + bannerHtmlId + "></div>" +
+										"</div>" +
+									 "</div>";
 
-                    $(".article__body > p:nth-of-type(6)").after(mobileMpuHtml);
-                    $("body").prepend(bannerHtml);
+					$(".article__body > p:nth-of-type(6)").after(mobileMpuHtml);
+					$("body").prepend(bannerHtml);
 
-                    googletag.cmd.push(function () {
-                        googletag.display(mobileMpuId);
-                        googletag.display(bannerHtmlId);
-                    });
-                }
-            }
-        },
+					googletag.cmd.push(function () {
+						googletag.display(mobileMpuId);
+						googletag.display(bannerHtmlId);
+					});
+				}
+			}
+		},
 
-        ready = function (config) {
-            if (!this.initialised) {
-                this.initialised = true;
-                
-                if (config.adsEnabled == "true") {
-                    modules.addGoogleTags(config);
-                    modules.insertAds();
-                } 
-                // console.info("Ads ready");
-            }
-        };
+		ready = function (config) {
+			if (!this.initialised) {
+				this.initialised = true;
+				
+				if (config.adsEnabled == "true") {
+					modules.addGoogleTags(config);
+					modules.insertAds();
+				} 
+				// console.info("Ads ready");
+			}
+		};
 
-    return {
-        init: ready
-    };
+	return {
+		init: ready
+	};
 
 });
