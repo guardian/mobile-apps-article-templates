81ac9c52691c879d12a34e0704a312b1015f5d9e
Sam Morris <mail@sammorrisdesign.co.uk>
2014-03-17 18:25:34 +0000

    Placeholder for gallery images before load and better spacing in between

--- ArticleTemplates/js/guardian.gallery.js
+++ ArticleTemplates/js/guardian.gallery.js
@@ -1,18 +1,23 @@
 var windowWidth = window.innerWidth;
 var imageHeight;
+var padding;
 
 if (windowWidth < 450 ) {
 	imageHeight = 150; 
+	padding = 4;
 } else {
 	imageHeight = 300;
+	padding = 8;
 }
 
 $('.gallery__thumbnails').collagePlus({
-        'targetHeight' : imageHeight
+        'targetHeight' : imageHeight,
+        'padding' : padding
     });
 
 $(window).resize(function() {
     $('.gallery__thumbnails').collagePlus({
-        'targetHeight' : imageHeight
+        'targetHeight' : imageHeight,
+        'padding' : padding
     });
 });
