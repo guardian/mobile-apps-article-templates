1c1740e2a15b5b5d7f55373b9ad95b2e978bd77c
Sam Morris <mail@sammorrisdesign.co.uk>
2014-03-17 18:27:42 +0000

    Removing padding from javascript

--- ArticleTemplates/js/guardian.gallery.js
+++ ArticleTemplates/js/guardian.gallery.js
@@ -4,20 +4,16 @@ var padding;
 
 if (windowWidth < 450 ) {
 	imageHeight = 150; 
-	padding = 4;
 } else {
 	imageHeight = 300;
-	padding = 8;
 }
 
 $('.gallery__thumbnails').collagePlus({
-        'targetHeight' : imageHeight,
-        'padding' : padding
+        'targetHeight' : imageHeight
     });
 
 $(window).resize(function() {
     $('.gallery__thumbnails').collagePlus({
-        'targetHeight' : imageHeight,
-        'padding' : padding
+        'targetHeight' : imageHeight
     });
 });
