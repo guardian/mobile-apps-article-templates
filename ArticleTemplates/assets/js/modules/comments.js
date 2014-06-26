e031413ee32e9abd54eef66577fd806f89e32be6
Paul Robert Lloyd <me+github@paulrobertlloyd.com>
2014-06-26 18:49:32 +0100

    Refactor how we include icons in document, so that VoiceOver doesn't read them out.

--- ArticleTemplates/assets/js/modules/comments.js
+++ ArticleTemplates/assets/js/modules/comments.js
@@ -25,7 +25,7 @@ define([
                                 if (numOfComments == 1) {
                                     $(this).addClass("block--discussion-thread--orphan");
                                 } else {
-                                    $(blockID).append("<div class='more more--comments'><span class='more__label' data-icon='&#xe050;'>" + numOfComments + " more replies</span></div>");
+                                    $(blockID).append("<div class='more more--comments'><span class='more__label'><span data-icon='&#xe050;' aria-hidden='true'></span>" + numOfComments + " more replies</span></div>");
                                 }
                             }
                         }
