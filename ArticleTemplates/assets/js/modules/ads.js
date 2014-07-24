25b3cc1f4f6242d5c752f57b706c0b24914ef7f1
Andrew Bulhak <andrew.bulhak@theguardian.com>
2014-07-24 17:48:32 +0100

    Removed the Google ad code; made ad spaces work as pure placeholders

--- ArticleTemplates/assets/js/modules/ads.js
+++ ArticleTemplates/assets/js/modules/ads.js
@@ -11,51 +11,8 @@ define([
         bannerHtmlId = 'advert-banner-content',
 
         modules = {
-            addGoogleTags: function (config) {
-                var googletag = window.googletag = window.googletag || {};
-                googletag.cmd = window.googletag.cmd || [];
-
-                var gads = document.createElement('script'),
-                    useSSL = 'https:' === document.location.protocol,
-                    node = document.getElementsByTagName('script')[0];
-
-                gads.async = true;
-                gads.type = 'text/javascript';
-                gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
-                node.parentNode.insertBefore(gads, node);
-
-                var windowWidth = window.innerWidth,
-                    bannerWidth = 900,
-                    bannerHeight = 250;
-
-                if (windowWidth < 300) {
-                    bannerWidth = 216;
-                    bannerHeight = 36;
-                }
-
-                if (windowWidth < 728 && windowWidth >= 300) {
-                    bannerWidth = 300;
-                    bannerHeight = 50;
-                }
-
-                if (windowWidth < 900 && windowWidth >= 728) {
-                    bannerWidth = 728;
-                    bannerHeight = 90;
-                }
-
-                googletag.cmd.push(function () {
-                    googletag.defineSlot(config.adsSlot, [[300, 250]], tabletMpuId).addService(googletag.pubads());
-                    googletag.defineSlot(config.adsSlot, [[300, 250]], mobileMpuId).addService(googletag.pubads());
-                    googletag.defineSlot(config.adsSlot, [[bannerWidth, bannerHeight]], bannerHtmlId).addService(googletag.pubads());
-                    googletag.pubads().enableSingleRequest();
-                    googletag.pubads().setTargeting('k', eval(config.adsKeywordTargeting));
-                    googletag.enableServices();
-                });
-            },
-
-            insertAds: function (config) {
-                var googletag = window.googletag,
-                    windowWidth = window.innerWidth;
+            insertAdPlaceholders: function (config) {
+                var windowWidth = window.innerWidth;
                 
                 var counter = 0;
                 
@@ -74,21 +31,18 @@ define([
                 if (config.adsConfig == "tablet" && counter == 3) {
                     var tabletMpuHtml = "<div class='advert-slot advert-slot--mpu advert-slot--mpu--tablet'>" +
                                             "<div class='advert-slot__label'>Advertisement</div>" +
-                                            "<div class='advert-slot__wrapper'>" +
+                                            "<div class=\"advert-slot__wrapper\" id=\"advert-slot__wrapper\">" +
                                                 "<div class='advert-slot__wrapper__content' id=" + tabletMpuId + "></div>" +
                                             "</div>" +
                                         "</div>";
 
                     $(".article__body > div > p:nth-of-type(1)").before(tabletMpuHtml);
 
-                    googletag.cmd.push(function () {
-                        googletag.display(tabletMpuId);
-                    });
 
                 } else if (config.adsConfig == "mobile") {
                     var mobileMpuHtml = "<div class='advert-slot advert-slot--mpu advert-slot--mpu--mobile'>" +
                                             "<div class='advert-slot__label'>Advertisement</div>" +
-                                            "<div class='advert-slot__wrapper'>" +
+                                            "<div class=\"advert-slot__wrapper\" id=\"advert-slot__wrapper\">" +
                                                 "<div class='advert-slot__wrapper__content' id=" + mobileMpuId + "></div>" +
                                             "</div>" +
                                         "</div>",
@@ -98,10 +52,6 @@ define([
                     $(".article__body > div > p:nth-of-type(6)").after(mobileMpuHtml);
                     $(".advert-slot__wrapper").prepend(bannerHtml);
 
-                    googletag.cmd.push(function () {
-                        googletag.display(mobileMpuId);
-                        googletag.display(bannerHtmlId);
-                    });
        
                 }
             },
@@ -111,7 +61,8 @@ define([
             // formatter(left, top, width, height)
 
             getMpuPos : function(formatter) {
-                var r, el = document.getElementsByClassName("advert-slot__wrapper__content")[0];
+                var r;
+                var el = document.getElementById("advert-slot__wrapper");
                 if (el) {
                     r = el.getBoundingClientRect();
                     return formatter(r.left + document.body.scrollLeft, r.top+document.body.scrollTop, r.width, r.height);
@@ -138,8 +89,7 @@ define([
                 this.initialised = true;
                 
                 if (config.adsEnabled == "true") {
-                    modules.addGoogleTags(config);
-                    modules.insertAds(config);
+                    modules.insertAdPlaceholders(config);
                 }
                 window.getMpuPosJson = modules.getMpuPosJson;
                 window.getMpuPosCommaSeparated = modules.getMpuPosCommaSeparated;
