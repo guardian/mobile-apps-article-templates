72081272dec5bb9606efdbc7900cd1482db23e10
Andrew Bulhak <andrew.bulhak@theguardian.com>
2014-08-01 16:40:44 +0100

    Removed redundant Google Tags code

--- ArticleTemplates/assets/js/modules/ads.js
+++ ArticleTemplates/assets/js/modules/ads.js
@@ -11,51 +11,8 @@ define([
         bannerHtmlId = 'advert-banner-content',
 
         modules = {
-            addGoogleTags: function (config) {
-                var googletag = window.googletag = window.googletag || {};
-                googletag.cmd = window.googletag.cmd || [];
-
-                var gads = document.createElement('script'),
-                    useSSL = 'https:' === document.location.protocol,
-                    node = document.getElementsByTagName('script')[0];
-
-                gads.async = true;
-                gads.type = 'text/javascript';
-                gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
-                node.parentNode.insertBefore(gads, node);
-
-                var windowWidth = window.innerWidth,
-                    bannerWidth = 900,
-                    bannerHeight = 250;
-
-                if (windowWidth < 300) {
-                    bannerWidth = 216;
-                    bannerHeight = 36;
-                }
-
-                if (windowWidth < 728 && windowWidth >= 300) {
-                    bannerWidth = 300;
-                    bannerHeight = 50;
-                }
-
-                if (windowWidth < 900 && windowWidth >= 728) {
-                    bannerWidth = 728;
-                    bannerHeight = 90;
-                }
-
-                googletag.cmd.push(function () {
-                    googletag.defineSlot(config.adsSlot, [[300, 250]], tabletMpuId).addService(googletag.pubads());
-                    googletag.defineSlot(config.adsSlot, [[300, 250]], mobileMpuId).addService(googletag.pubads());
-                    googletag.defineSlot(config.adsSlot, [[bannerWidth, bannerHeight]], bannerHtmlId).addService(googletag.pubads());
-                    googletag.pubads().enableSingleRequest();
-                    googletag.pubads().setTargeting('k', eval(config.adsKeywordTargeting));
-                    googletag.enableServices();
-                });
-            },
-
             insertAds: function (config) {
-                var googletag = window.googletag,
-                    windowWidth = window.innerWidth;
+                var windowWidth = window.innerWidth;
                 
                 var counter = 0;
                 
@@ -81,9 +38,6 @@ define([
 
                     $(".article__body > div > p:nth-of-type(1)").before(tabletMpuHtml);
 
-                    googletag.cmd.push(function () {
-                        googletag.display(tabletMpuId);
-                    });
 
                 } else if (config.adsConfig == "mobile") {
                     var mobileMpuHtml = "<div class='advert-slot advert-slot--mpu advert-slot--mpu--mobile'>" +
@@ -97,11 +51,6 @@ define([
 
                     $(".article__body > div > p:nth-of-type(6)").after(mobileMpuHtml);
                     $(".advert-slot__wrapper").prepend(bannerHtml);
-
-                    googletag.cmd.push(function () {
-                        googletag.display(mobileMpuId);
-                        googletag.display(bannerHtmlId);
-                    });
        
                 }
             },
@@ -138,7 +87,6 @@ define([
                 this.initialised = true;
                 
                 if (config.adsEnabled == "true") {
-                    modules.addGoogleTags(config);
                     modules.insertAds(config);
                 }
                 window.getMpuPosJson = modules.getMpuPosJson;
