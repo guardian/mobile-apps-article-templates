00ad9117f7b6a2424df515ab87dad13262d0900a
Justin Glueck <justin.glueck@guardian.co.uk>
2014-09-22 18:16:55 +0100

    GIA-3147 - A per task - 'It would be better if the header was loaded by default with NO text on the right hand side to indicate open/closed, and then when discussion has responded, the correct text is inserted into the header' - this is to avoid a flash on slower devices which intially shows the leave a comment message then changes to Comments Closed (where Comments are closed). To achieve this I've added a new function commentsOpen which adds the comments--open class and cascades to display this message when inserted into the DOM. This also required a change in iOS native code to trigger the commentsOpen function (I will need a similar function on Android native but I need help for this). I've also added a height to the containing div .container__header to prevent the movement of the text which occurred before when the messages changed. Finally changed the closed comments message in article view to appear below the number of Comments h2.

--- ArticleTemplates/assets/js/modules/comments.js
+++ ArticleTemplates/assets/js/modules/comments.js
@@ -116,6 +116,11 @@ define([
                 };
                 window.commentsClosed = function () {
                     $("#comments, #discussion").addClass("comments--closed");
+                    window.logOnScreen("added comments--closed");
+                };
+                window.commentsOpen = function () {
+                    $("#comments, #discussion").addClass("comments--open");
+                    window.logOnScreen("added comments--open");
                 };
                 window.commentTime = function () {
                     relativeDates.init('.comment__timestamp', 'title');
@@ -138,6 +143,7 @@ define([
                 window.applyNativeFunctionCall('articleCommentsInserter');
                 window.applyNativeFunctionCall('commentsFailed');
                 window.applyNativeFunctionCall('commentsClosed');
+                window.applyNativeFunctionCall('commentsOpen');
                 window.applyNativeFunctionCall('articleCommentsFailed');
             }
         },
