22ced9b121ba48c94fdee1772dbf59692beeb7d3
jglueck <justin.glueck@guardian.co.uk>
2014-12-22 17:22:51 +0000

    GIA-3300 - Changed id to class in JS selector for running show more tags module, removed tags id from templates so just class remained, added section container to Article template for new series tag structure and tweaked section_container/child element styles

--- ArticleTemplates/assets/js/modules/more-tags.js
+++ ArticleTemplates/assets/js/modules/more-tags.js
@@ -15,27 +15,20 @@ define([
     function MoreTags() {
 
         this.init = function () {
-            var tags = $("#tags .inline-list__item a");
-<<<<<<< HEAD
-            var string = "<li class='inline-list__item more-button js-more-button'><a href='#' id='more'>more</a></li>";
-            // var el = $("#more"); // *** Used for Bean not working ***
-=======
+
+            var tags = $(".tags .inline-list__item a");
             var string = "<li class='inline-list__item more-button js-more-button'><a id='more'>more</a></li>";
             // var el = $(".js-more-button"); // *** Used for Bean not working ***
->>>>>>> GIA-3300 - Tags fix for iOS, delay timing of loop for filtering tags
             var i = 0;
-
+            var more;
             // window.logOnScreen("Tags are "+tags);
             // window.logOnScreen("String is "+string);
             function showTags(tags, el) {
                 var i = 0;
                 // window.logOnScreen("EL "+el);
                 tags.each(function (index) {
-                    console.log("Looping"+i);
-<<<<<<< HEAD
-=======
-                    window.logOnScreen("Looping"+i);
->>>>>>> GIA-3300 - Tags fix for iOS, delay timing of loop for filtering tags
+                    // console.log("Looping"+i);
+                    // window.logOnScreen("Looping"+i);
                     if (i >= 5) {
                     $(this).parent().addClass("show-tags");
                     }
@@ -47,29 +40,34 @@ define([
 
             tags.each(function () {
                 // console.log("Module More Tags is Looping "+i);
-                window.logOnScreen("Module More Tags is Looping "+i);
+                // window.logOnScreen("Module More Tags is Looping "+i);
                 if (i > 4) {
                     // console.log("5 "+ i);
-                    window.logOnScreen("5 "+ i);
+                    // window.logOnScreen("5 "+ i);
                     string = bonzo.create(string);
                     $(string).insertAfter($(this).parent());
+                    more = true;
+                    
                 } 
 
                 if (i >= 5) {
                     $(this).parent().addClass("hide-tags");
                     // console.log("6+ "+ i);
-                    window.logOnScreen("6 "+ i);
+                    // window.logOnScreen("6 "+ i);
                 }
                 i++;
+                return more;
+
             });
 
             // *** Bean On click not working correctly, using native JS ***
             // bean.on(el, 'click', function (tags) {
             //     showTags(tags);
             // });
-            var el = document.getElementById("more").parentNode;
-            el.addEventListener("click", function(){ showTags(tags, el); }, false);
-
+            if (more === true) {
+                var el = document.getElementById("more").parentNode;
+                el.addEventListener("click", function(){ showTags(tags, el); }, false);
+            }
         };
 
     }
