fae740c562d5140583e44dab475643f559851ff7
Sandro Paganotti <sandro.paganotti@theguardian.com>
2015-01-08 15:19:24 +0000

    GIA-3321 commented out some debug options

--- ArticleTemplates/assets/build/build.js
+++ ArticleTemplates/assets/build/build.js
@@ -5,15 +5,11 @@
     out: "app.js",
 
     // use this to avoid compression while debugging
-    uglify: {
-        toplevel: true,
-        ascii_only: true,
-        beautify: true,
-        max_line_length: 120,
-
-        //Custom value supported by r.js but done differently
-        //in uglifyjs directly:
-        //Skip the processor.ast_mangle() part of the uglify call (r.js 2.0.5+)
-        no_mangle: true
-    }
+    // uglify: {
+    //     toplevel: true,
+    //     ascii_only: true,
+    //     beautify: true,
+    //     max_line_length: 120,
+    //     no_mangle: true
+    // }
 })
