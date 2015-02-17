1dac41888a12f9d8dbf3cf3fdf76e802913a8e01
Sandro Paganotti <sandro.paganotti@theguardian.com>
2015-02-17 16:45:40 +0000

    AND-3484: preparation work

--- test/server.js
+++ test/server.js
@@ -8,6 +8,22 @@ app.use('/performances', express.static(__dirname + '/../Performance'));
 app.use('/root', express.static(__dirname + '/..'));
 
 // forward every /mapi to production mapi
-app.use('/', proxy('http://mobile-apps.guardianapis.com'));
+app.use('/', proxy('http://mobile-apps.guardianapis.com', {
+	intercept: function(data){
+       data = JSON.parse(data.toString('utf8'));
+       console.log(data);
+       callback(null, JSON.stringify(data));
+	}
+}));
 
-module.exports = app;
+//module.exports = app;
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
