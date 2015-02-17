cc92baf20cb3fd4262a54209d7aa0ccbf66fc02a
Sandro Paganotti <sandro.paganotti@theguardian.com>
2015-02-17 15:44:10 +0000

    AND-3484: adding network proxy

--- test/server.js
+++ test/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var proxy = require('express-http-proxy');
 var app = express();
 
 app.use(express.static(__dirname + '/fixture'));
 app.use('/data/data/com.guardian/files/dev-templates/assets', express.static(__dirname + '/../ArticleTemplates/assets'));
 app.use('/performances', express.static(__dirname + '/../Performance'));
 app.use('/root', express.static(__dirname + '/..'));
+
+// forward every /mapi to production mapi
+app.use('/', proxy('http://mobile-apps.guardianapis.com'));
 
 module.exports = app;
