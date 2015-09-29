a523f794cae05f7f2c9d6f3308de73ed3d710070
Christine Ramsden <christabella76@gmail.com>
2015-09-29 11:13:32 +0100

    GIA-3814: WIP on related cards

--- ArticleTemplates/assets/js/modules/cards.js
+++ ArticleTemplates/assets/js/modules/cards.js
@@ -17,18 +17,18 @@ define([
                     if (!html) {
                         $(".container--related").hide();
                     } else {
-                        $(".container--related .container__body").html(html);
-                        var comment_card_byline = $(".card--comment.has-image .card__byline");
-                        var comment_card_header = $(".card--comment.has-image .card__titletext");
-                        if (comment_card_byline.length>0)
-                        {
-                            for (var i=0;i<comment_card_byline.length;i++)
-                            {
-                                var  bylineLines=modules.getNumberOfTextLines(comment_card_byline[i]);
-                                var  headerLines=modules.getNumberOfTextLines(comment_card_header[i]);
-                                var  totalLines=bylineLines+headerLines;
-                                if (totalLines>=4)
-                                {
+                        $(".container--related .cards").html('<ul class="cards__list" id="related-content-body">' + html + '</ul>');
+                        
+                        var comment_card_byline = $(".card--comment.has-image .card__byline"),
+                            comment_card_header = $(".card--comment.has-image .card__titletext");
+                        
+                        if (comment_card_byline.length > 0) {
+                            for (var i=0; i < comment_card_byline.length; i++) {
+                                var bylineLines = modules.getNumberOfTextLines(comment_card_byline[i]),
+                                    headerLines = modules.getNumberOfTextLines(comment_card_header[i]),
+                                    totalLines = bylineLines + headerLines;
+
+                                if (totalLines >= 4) {
                                     bonzo(comment_card_byline[i]).hide();
                                     bonzo(comment_card_header[i]).attr('style','-webkit-line-clamp:4;');
                                 }
@@ -40,12 +40,13 @@ define([
             },
             getNumberOfTextLines:function (el) {
                 //returns number of text lines of single html element
-                var cssValues=window.getComputedStyle(el,null);
-                var lineHeight=cssValues.lineHeight.replace('px','');
-                var paddingTop=cssValues.paddingTop.replace('px','');
-                var paddingBottom=cssValues.paddingBottom.replace('px','');
-                var elementheight=el.offsetHeight;
-                var numberOfLines= (elementheight-paddingTop-paddingBottom)/lineHeight;
+                var cssValues = window.getComputedStyle(el, null),
+                    lineHeight = cssValues.lineHeight.replace('px', ''),
+                    paddingTop = cssValues.paddingTop.replace('px', ''),
+                    paddingBottom = cssValues.paddingBottom.replace('px', ''),
+                    elementHeight = el.offsetHeight,
+                    numberOfLines = (elementHeight - paddingTop - paddingBottom) / lineHeight;
+
                 return parseInt(numberOfLines);
             }
         },
@@ -54,7 +55,6 @@ define([
             if (!this.initialised) {
                 this.initialised = true;
                 modules.setupGlobals();
-                // console.info("Cards ready");
             }
         };
 
