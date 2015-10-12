eac2a693a5c20f302f24245ad81390fa7253d6b6
David Simkin <david.simkin@guardian.co.uk>
2015-10-12 12:51:12 +0100

    Add a method for updating video positions if the page height changes
    Conflicts:
    	ArticleTemplates/assets/js/bootstraps/common.js

--- ArticleTemplates/assets/js/bootstraps/common.js
+++ ArticleTemplates/assets/js/bootstraps/common.js
@@ -186,7 +186,7 @@ define([
         },
 
         videoPositioning: function () {
-            window.videoPositioning = function (html) {
+            window.videoPositioning = function () {
                 var mainMedia = $('.video-URL');
                 if (mainMedia) {
                     for (var i = mainMedia.length - 1; i >= 0; i--) {
@@ -194,8 +194,18 @@ define([
                         window.GuardianJSInterface.videoPosition(media.offset().left, media.offset().top, media.offset().width, media.attr('href'));
                     }
                 }
+                setTimeout(modules.videoPositioningPoller.bind(window.innerHeight), 500);
             };
             window.applyNativeFunctionCall('videoPositioning');
+        },
+
+        videoPositioningPoller: function(pageHeight) {
+            var newHeight = window.innerHeight;
+            if(pageHeight != newHeight) {
+                window.videoPositioning();
+            } else {
+                setTimeout(modules.videoPositioningPoller.bind(newHeight), 500);
+            }  
         },
 
         offline: function() {
