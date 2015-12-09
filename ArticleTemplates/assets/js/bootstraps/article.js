9a39f3dbeb8e1f78643d60ee72e50f04682d91ad
Christine Ramsden <christabella76@gmail.com>
2015-12-09 10:27:05 +0000

    DES-8: Ongoing work for the new quiz template

--- ArticleTemplates/assets/js/bootstraps/article.js
+++ ArticleTemplates/assets/js/bootstraps/article.js
@@ -42,13 +42,50 @@ define([
 
             // Loop through every question and set up the answers and click events for it's answers
             $('.quiz__question').each(function(question, index) {
-                // This questions correct answer
+                // Wrap question in a div for styling
+                var questionWrapper = document.createElement('div'),
+                    questionAnswerList = question.querySelectorAll('.question__answers');
+                $(questionWrapper).addClass('question__wrapper');
+                question.insertBefore(questionWrapper, questionAnswerList[0]);
+                // Does this question have an image
+                var questionImg = question.querySelectorAll('.question__img');
+                if (questionImg.length) {
+                    $(question).addClass('hasImage');
+                    $(questionWrapper).append(questionImg);
+                }
+                // Does this question have text
+                var questionText = question.querySelectorAll('.question__text');
+                if (questionText.length) {
+                    $(questionWrapper).append(questionText);
+                }
+
+                // This question's correct answer
                 var correctAnswer = answers[index].split(':')[1].toUpperCase();
                 
                 // All the answers for this question
                 var questionAnswers = this.querySelectorAll('.question__answer');
 
                 $(questionAnswers).each(function(answer, index) {
+                    // Wrap answer in a div for styling
+                    var answerWrapper = document.createElement('div');
+                    $(answerWrapper).addClass('answer__wrapper');
+                    $(answer).append(answerWrapper);
+                    // Add a marker icon span 
+                    var answerMarker = document.createElement('div');
+                    $(answerMarker).addClass('answer__marker');
+                    $(answerWrapper).append(answerMarker);
+                    // Does this answer have an image
+                    var answerImg = answer.querySelectorAll('.answer__img');
+                    if (answerImg.length) {
+                        $(answer).addClass('hasImage');
+                        $(answerWrapper).append(answerImg);
+                    }
+                    // Does this answer have text
+                    var answerText = answer.querySelectorAll('.answer__text');
+                    if (answerText.length) {
+                        $(answerWrapper).append(answerText);
+                    }
+
                     // Find this answers alpha key
                     var thisAnswer = String.fromCharCode(65 + index);
 
@@ -68,7 +105,17 @@ define([
                             $(this).addClass('wrong');
                         }
 
-                        $(this).append('<p class="answer__explanation">This is the answer explanation, which will show up after either the correct or wrong answer</p>');
+                        $(answerWrapper).append('<p class="answer__explanation">This is the answer explanation, which will show up after either the correct or wrong answer</p>');
+
+                        // When we have an image answer we need to move the positioning of the explanation and marker 
+                        if (answerImg.length) {
+                            var answerExplanation = answer.querySelectorAll('.answer__explanation')[0],
+                                answerExplanationHeight = answerExplanation.offsetHeight;
+                                answerMarker = answer.querySelectorAll('.answer__marker')[0];
+                            // position explanation to the bottom of wrapper
+                            answerExplanation.style.top = 'calc(100% - ' + answerExplanationHeight + 'px)';
+                            answerMarker.style.top = 'calc(100% - ' + (answerExplanationHeight - 7) + 'px)';
+                        }
 
                         if (numQuestions == numAnswered) {
                             $('.quiz__scores').html('<span class="quiz__score">' + score +'</span> out of <span class="quiz__number-questions">' + numQuestions + '</span>');
