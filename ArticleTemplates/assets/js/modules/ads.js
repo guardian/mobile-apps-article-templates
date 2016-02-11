5f4b3d22074bf8e2631eb6f006d743e6b0e9147f
Christine Ramsden <christabella76@gmail.com>
2016-02-11 17:19:02 +0000

    DES-36: Update ads logic to allow up to 2 mpus in liveblogs

--- ArticleTemplates/assets/js/modules/ads.js
+++ ArticleTemplates/assets/js/modules/ads.js
@@ -8,42 +8,17 @@ define([
 ) {
     'use strict';
 
-    var tabletMpuId = 'advert-mpu-content',
-        mobileMpuId = 'advert-mobile-mpu-content',
-        liveblogAdId = 0,
+    var numberOfMpus = 0,
+        adsConfig,
 
         modules = {
             insertAdPlaceholders: function (config) {
-                var counter = 0,
-                    mpuId = '',
-                    mpuHtml = '';
+                numberOfMpus = 1;
+                var mpuHtml = modules.createMpuHtml(numberOfMpus);
 
-                $('.article__body > div > *:nth-child(-n+3)').each(function() {
-                    var tagName = $(this)[0].tagName;
-
-                    if (tagName == 'P' || 
-                        tagName == 'H2' || 
-                        tagName == 'BLOCKQUOTE' ||
-                        (tagName == 'FIGURE' && $(this).hasClass('element-placeholder')) ||
-                        $(this).hasClass('element-video')) {
-                        counter++;
-                    }
-                });
-
-                // With split screen now available on ios, tablets can now end up being displayed in the mobile format.
-                // To overcome this decide mpu placement with css positioning instead of appending in different places
-                if (config.adsConfig == 'tablet' && counter == 3) {
-                    mpuId = tabletMpuId;
-                } else if (config.adsConfig == 'mobile') {
-                    mpuId = mobileMpuId;
-                }
-
-                if (mpuId !== '') {
-                    mpuHtml = modules.createMpuHtml(mpuId);
-                    // To mimic the correct positioning on full width tablet view, we will need an 
-                    // empty div to pad out the text so we can position absolutely over it.
-                    $('.article__body > div.prose > :first-child').before('<div class="advert-slot advert-slot--placeholder"></div>');
-                }
+                // To mimic the correct positioning on full width tablet view, we will need an 
+                // empty div to pad out the text so we can position absolutely over it.
+                $('.article__body > div.prose > :first-child').before('<div class="advert-slot advert-slot--placeholder"></div>');
 
                 var nrParagraph = ( parseInt(config.mpuAfterParagraphs, 10) || 6 ) - 1;
                 $('.article__body > div.prose > p:nth-of-type(' + nrParagraph + ') ~ p + p').first().before(mpuHtml);
@@ -56,8 +31,8 @@ define([
                     $(blocks).each(function(block, index) {
                         // insert an advert after every 2nd and 7th block
                         if (index === 1 || index === 6) {
-                            var id = 'mpu' + liveblogAdId++,
-                                mpuHtml = modules.createMpuHtml(id);
+                            numberOfMpus++;
+                            var mpuHtml = modules.createMpuHtml(numberOfMpus);
                             $(block).after(mpuHtml);
                         }
                     });
@@ -83,81 +58,84 @@ define([
                         '</div>';
             },
 
-            // return the current MPU's position.
+            // return the position of all mpus.
             // This function is an internal function which accepts a function
-            // formatter(left, top, width, height)
+            // formatter(left1, top1, width1, height1, left2, top2, width2, height2)
             getMpuPos: function(formatter) {
-                var r;
-                var el = document.getElementByClassName('advert-slot__wrapper')[0];
-                if (el) {
-                    r = el.getBoundingClientRect();
-                    if(r.width !== 0 && r.height !== 0){
-                        return formatter(r.left + document.body.scrollLeft,
-                            r.top+document.body.scrollTop, r.width, r.height);
-                    }
-                } else {
-                    return null;
-                }
-            },
-
-            getLiveblogMpuPos: function(formatter) {
                 var $advertSlots = $('.advert-slot__wrapper');
 
                 if ($advertSlots.length) {
                     var scrollLeft = document.body.scrollLeft,
                         scrollTop = document.body.scrollTop,
-                        params;
+                        params = [];
 
                     $advertSlots.each(function(ad, index) {
-                        var coords = ad.getBoundingClientRect();
+                        var adPos = ad.getBoundingClientRect();
+                        if (adPos.width !== 0 && adPos.height !== 0) {
+                            params.push(adPos.left + scrollLeft);
+                            params.push(adPos.top + scrollTop);
+                            params.push(adPos.width);
+                            params.push(adPos.height);
+                        }
                     });
-                } else {
-                    return null;
-                }
 
-                if (el) {
-                    r = el.getBoundingClientRect();
-                    if(r.width !== 0 && r.height !== 0){
-                        return formatter(r.left + document.body.scrollLeft,
-                            r.top+document.body.scrollTop, r.width, r.height);
+                    if (params.length > 4) {
+                        return formatter(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7]);
+                    } else {
+                        return formatter(params[0], params[1], params[2], params[3], -1, -1, -1, -1);
                     }
                 } else {
                     return null;
                 }
             },
 
-            getMpuPosJson: function() {
-                return modules.getMpuPos(function(x, y, w, h) {
-                    return '{"left":' + x + ', "top":' + y + ', "width":' + w +', "height":' + h + '}';
-                });
-            },
-
             getMpuPosCommaSeparated: function() {
-                return modules.getMpuPos(function(x, y, w, h) {
-                    return x + ',' + y;
+                return modules.getMpuPos(function(x1, y1, w1, h1, x2, y2, w2, h2) {
+                    if (numberOfMpus > 1) {
+                        return x1 + ',' + y1 + ',' + x2 + ',' + y2;
+                    } else {
+                        return x1 + ',' + y1;
+                    }
                 });
             },
 
-            getMpuOffsetTop: function() {
-                return modules.getMpuPos(function(x, y, w, h) {
-                    return y;
+            getMpuOffset: function() {
+                return modules.getMpuPos(function(x1, y1, w1, h1, x2, y2, w2, h2) {
+                    if (numberOfMpus > 1) {
+                        return x1 + "-" + y1 + ":" + x2 + "-" + y2;
+                    } else {
+                        return x1 + "-" + y1;
+                    }
                 });
             },
 
-            getLiveblogMpuOffsetTop:  function() {
-                return modules.getLiveblogMpuPos(function(x, y, w, h) {
-                    return y;
-                });
+            updateAndroidPosition : function() {
+                if (adsConfig === 'liveblog') {
+                    modules.getMpuPos(function(x1, y1, w1, h1, x2, y2, w2, h2){
+                        window.GuardianJSInterface.mpuLiveblogAdsPosition(x1, y1, w1, h1, x2, y2, w2, h2);
+                    });
+                } else {
+                    modules.getMpuPos(function(x1, y1, w1, h1, x2, y2, w2, h2){
+                        window.GuardianJSInterface.mpuAdsPosition(x1, y1, w1, h1);
+                    });
+                }
             },
 
-            poller: function(interval, yPos, firstRun) {
-                var newYPos = modules.getMpuOffsetTop();
+            initMpuPoller: function(){
+                modules.poller(1000,
+                    modules.getMpuOffset(),
+                    true
+                );
+            },
+
+            poller: function(interval, adPositions, firstRun) {
+                var newAdPositions = modules.getMpuOffset();
 
                 if(firstRun && this.isAndroid){
                     modules.updateAndroidPosition();
                 }
 
-                if(newYPos !== yPos){
+                if(newAdPositions !== adPositions){
                     if(this.isAndroid){
                         modules.updateAndroidPosition();
                     } else {
@@ -165,27 +143,16 @@ define([
                     }
                 }
 
-                setTimeout(modules.poller.bind(modules, interval + 50, newYPos), interval);
+                setTimeout(modules.poller.bind(modules, interval + 50, newAdPositions), interval);
             },
 
-            liveblogPoller: function(interval, yPosArray, firstRun) {
-                if(firstRun && this.isAndroid){
-                    modules.updateAndroidPosition();
+            fireAdsReady: function(_window) {
+                if (!$('body').hasClass('no-ready') && $('body').attr('data-use-ads-ready') === 'true') {
+                    _window.location.href = 'x-gu://ads-ready';
                 }
-
-                var newYPos = modules.getMpuOffsetTop();
-
-                if(newYPos !== yPos){
-                    if(this.isAndroid){
-                        modules.updateAndroidLiveblogPosition();
-                    } else {
-                        window.location.href = 'x-gu://ad_moved';
-                    }
-                }
-
-                setTimeout(modules.liveblogPoller.bind(modules, interval + 50, newYPosArray), interval);
             },
 
+            // Used by quizzes to move the adverts
             updateMPUPosition: function(yPos) {
                 if (!yPos) {
                     yPos = $('.advert-slot__wrapper').first().offset().top;
@@ -202,39 +169,6 @@ define([
                 }
 
                 return newYPos;
-            },
-
-            updateAndroidPosition : function() {
-                modules.getMpuPos(function(x, y, w, h){
-                    window.GuardianJSInterface.mpuAdsPosition(x, y, w, h);
-                });
-            },
-
-            updateAndroidLiveblogPosition : function() {
-                modules.getLiveblogMpuPos(function(x1, y1, w1, h1, x2, y2, w2, h2){
-                    window.GuardianJSInterface.mpuLiveblogAdsPosition(x1, y1, w1, h1, x2, y2, w2, h2);
-                });
-            },
-
-            initMpuPoller: function(){
-                modules.poller(1000,
-                    modules.getMpuOffsetTop(),
-                    true
-                );
-            },
-
-            initMpuLiveblogPoller: function(){
-                modules.liveblogPoller(1000,
-                    modules.getLiveblogMpuOffsetTop(),
-                    true
-                );
-            },
-
-            fireAdsReady: function(_window) {
-                if (!$('body').hasClass('no-ready') && $('body').attr('data-use-ads-ready') === 'true') {
-                    console.log('test');
-                    _window.location.href = 'x-gu://ads-ready';
-                }
             }
         },
 
@@ -245,16 +179,17 @@ define([
                 this.initialised = true;
 
                 if (config.adsEnabled == 'true' || (config.adsEnabled !== null && config.adsEnabled.match && config.adsEnabled.match(/mpu/))) {
+                    // Insert advert placeholders article & liveblog
                     if (config.contentType === 'liveblog') {
                         modules.insertLiveblogAdPlaceholders();
-                        window.initMpuLiveblogPoller = modules.initMpuLiveblogPoller;
-                        window.applyNativeFunctionCall('initMpuLiveblogPoller');
+                        adsConfig = 'liveblog';
                     } else {
                         modules.insertAdPlaceholders(config);
-                        window.initMpuPoller = modules.initMpuPoller;
-                        window.applyNativeFunctionCall('initMpuPoller');
+                        adsConfig = 'default';
                     }
-                    window.getMpuPosJson = modules.getMpuPosJson;
+
+                    window.initMpuPoller = modules.initMpuPoller;
+                    window.applyNativeFunctionCall('initMpuPoller');
                     window.getMpuPosCommaSeparated = modules.getMpuPosCommaSeparated;
 
                     if(!modules.isAndroid){
