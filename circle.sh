65660bef2bfe95bc6e8597883af982e1f9b9138c
George Haberis <georgioshaberis@gmail.com>
2016-12-09 16:23:15 +0000

    Test circle.sh fails

--- circle.sh
+++ circle.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/sh -xe
 
-git status
-git log -1
-git config --global user.name "GuardianAndroid"
-git config --global user.email "guardian.android@gmail.com"
-git config --global push.default simple
-git commit -a -m "Generate files for release [skip ci]"
-git checkout master
-git fetch
-git reset --hard origin/master
-git log -1
-git merge release
-git push origin master
-git push origin release
+# check if there are changes
+# if [[ `git status --porcelain` ]]; then
+  	git status
+	git log -1
+	git config --global user.name "GuardianAndroid"
+	git config --global user.email "guardian.android@gmail.com"
+	git config --global push.default simple
+	git commit -a -m "Generate files for release [skip ci]"
+	git checkout master
+	git fetch
+	git reset --hard origin/master
+	git log -1
+	git merge release
+	git push origin master
+	git push origin release
+# fi
