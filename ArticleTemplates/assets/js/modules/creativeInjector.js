5ca0cfdedd5c47bfec97d9092c3147ef6b502f54
George Haberis <georgioshaberis@gmail.com>
2017-02-10 18:44:41 +0000

    update to handle inline creatives

--- ArticleTemplates/assets/js/modules/creativeInjector.js
+++ ArticleTemplates/assets/js/modules/creativeInjector.js
@@ -13,10 +13,10 @@ define(function () {
         }
     }
 
-    function injectInlineCreative(html, css, id) {
-        if (GU.util.isOnline() && !document.getElementById(id)) {    
+    function injectInlineCreative(html, css, id, type) {
+        if (GU.util.isOnline() && !document.getElementById(id)) {   
             injectCSS(css);
-            injectHTML(html, id);
+            injectHTML(html, id, type);
         }
     }
 
@@ -34,25 +34,53 @@ define(function () {
         document.head.appendChild(style);
     }
 
-    function injectHTML(html, id) {
-        var prose = document.querySelector('.article__body > div.prose'),
-            inlineCreativeContainer = document.createElement('div');
+    function injectHTML(html, id, type) {
+        var creativeContainer = document.createElement('div');
         
-        inlineCreativeContainer.classList.add('inline-creative-container');
-        inlineCreativeContainer.id = id;
-        inlineCreativeContainer.innerHTML = html;
+        creativeContainer.classList.add(type + '-creative-container');
+        creativeContainer.id = id;
+        creativeContainer.innerHTML = html;
 
-        if (prose) {
-            prose.appendChild(inlineCreativeContainer);
-            // on scroll check if creative is in viewport
-            window.addEventListener('scroll', GU.util.debounce(isInlineCreativeInView.bind(null, inlineCreativeContainer, id), 100));
+        if (type === 'inline-article') {
+            injectInlineCreative(creativeContainer);
+        } else {
+            injectEpicCreative(creativeContainer);
         }
     }
 
-    function isInlineCreativeInView(inlineCreativeContainer, id) {
+    function injectInlineCreative(creativeContainer) {
+        var i,
+            prose = document.querySelector('.article__body > div.prose'),
+            paragraphs = prose.querySelectorAll('p');
+
+        if (paragraphs.length < 4) {
+            return;
+        }
+
+        // loop through paragraphs after 3rd paragraph 
+        // insert creativeContainer after any paragraph which is followed by a paragraph or header
+        for (i = 3; i < paragraphs.length; i++) {
+            if (paragraphs[i].nextSibling && 
+                (paragraphs[i].nextSibling.tagName === 'P' || paragraphs[i].nextSibling.tagName === 'H1')) {
+                paragraphs[i].nextSibling.parentNode.insertBeforeElem(creativeContainer, paragraphs[i].nextSibling);
+                break;
+            }
+        }
+    }
+
+    function injectEpicCreative(creativeContainer) {
+        var prose = document.querySelector('.article__body > div.prose');
+            
+        if (prose) {
+            prose.appendChild(creativeContainer);
+            window.addEventListener('scroll', GU.util.debounce(isInlineCreativeInView.bind(null, creativeContainer, id), 100));
+        }
+    }
+
+    function isInlineCreativeInView(creativeContainer, id) {
         var messageName = 'inline_creative_view/' + id;
 
-        if (trackedImpressions.indexOf(id) === -1 && GU.util.isElementPartiallyInViewport(inlineCreativeContainer)) {
+        if (trackedImpressions.indexOf(id) === -1 && GU.util.isElementPartiallyInViewport(creativeContainer)) {
             GU.util.signalDevice(messageName);
             
             trackedImpressions.push(id);
