89928d37574652ed4a8962153c2598b7194ff4bd
NataliaLKB <natalialeebaltazar@gmail.com>
2017-05-11 10:21:52 +0100

    editing git commands for release

--- circle.sh
+++ circle.sh
@@ -3,16 +3,11 @@
 # check if there are changes
 if [[ `git status --porcelain` ]]; then
   	git status
-	git log -1
-	git config --global user.name "GuardianAndroid"
-	git config --global user.email "guardian.android@gmail.com"
-	git config --global push.default simple
-	git commit -a -m "Generate files for release [skip ci]"
-	git checkout master
-	git fetch
-	git reset --hard origin/master
-	git log -1
-	git merge release
-	git push origin master
-	git push origin release
+  	git log -1
+  	git config --global user.name "GuardianAndroid"
+  	git config --global user.email "guardian.android@gmail.com"
+  	git config --global push.default simple
+  	git commit -a -m "Generate files for release [skip ci]"
+    git push origin release
+    git push origin master
 fi
