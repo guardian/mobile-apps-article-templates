5504c9aa2af5964ef74610704563991defe6dd68
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2017-08-04 15:05:27 +0100

    Use master branch on Android

--- circle.sh
+++ circle.sh
@@ -27,10 +27,10 @@ git add package.json
 git commit -m "$(printf "Update to mobile-apps-article-templates version $PACKAGE_VERSION")"
 git push origin master
 cd ..
-git clone -b npm-templates git@github.com:guardian/android-news-app.git
+git clone git@github.com:guardian/android-news-app.git
 cd android-news-app
 jq ".dependencies[\"@guardian/mobile-apps-article-templates\"] = \"${PACKAGE_VERSION}\"" package.json > tmp
 mv tmp package.json
 git add package.json
 git commit -m "$(printf "Update to mobile-apps-article-templates version $PACKAGE_VERSION")"
-git push origin npm-templates
+git push origin master
