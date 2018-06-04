66483408d57d7c01f86510a6aa129319d0fef023
GHaberis <george.haberis@theguardian.com>
2018-06-04 10:52:51 +0100

    tidy up

--- Gruntfile.js
+++ Gruntfile.js
@@ -234,7 +234,4 @@ module.exports = function(grunt) {
     grunt.registerTask('buildCSS', ['sass', 'cssmin']);
 
     grunt.registerTask('default', ['buildJS', 'buildCSS', 'connect', 'watch']);
-
-
-
 };
