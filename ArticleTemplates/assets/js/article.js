d0b14502ff5901eb2c430316b72c1f010dda7a44
GHaberis <george.haberis@theguardian.com>
2018-06-06 12:31:44 +0100

    remove old article code

--- ArticleTemplates/assets/js/article.js
+++ ArticleTemplates/assets/js/article.js
@@ -11,25 +11,3 @@ const init = () => {
 export {
     init,
 };
-
-// define([
-// 	'bootstraps/common',
-//     'bootstraps/article',
-//     'bootstraps/atoms'
-// ], function (
-// 	common,
-//     article,
-//     atoms
-// ) {
-//     'use strict';
-    
-//     function init() {
-//         common.init();
-//         article.init();
-//         atoms.init();
-//     }
-
-//     return {
-//         init: init
-//     };
-// });
