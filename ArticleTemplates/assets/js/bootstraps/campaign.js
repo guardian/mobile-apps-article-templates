5b231ee0ebe8fc76ec05b7d219dd299338b7d2dd
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-06 11:45:56 +0100

    Add JS

---
+++ ArticleTemplates/assets/js/bootstraps/campaign.js
@@ -0,0 +1,35 @@
+define([], function () {
+    'use strict';
+
+    function init() {
+      const campaign = document.querySelector('.campaign--snippet form');
+      if (campaign) {
+        initCampaign(campaign);
+      }
+    }
+
+    function initCampaign(campaign) {
+      campaign.onSubmit(() => {
+        const data = new FormData(campaign);
+        // TODO form submission URL
+        const request = new XMLHttpRequest('POST', '#');
+        request.onload(displayConfirmation.bind(null, campaign));
+        request.onerror(displayError.bind(null, campaign));
+        request.send(data);
+      });
+    }
+
+    function displayConfirmation(campaign) {
+      campaign.innerHTML = '<p>Thank you for your contribution</p>';
+    }
+
+    function displayError(campaign) {
+      if (! (campaign.firstChild.nodeType == Node.ELEMENT_NODE && campaign.firstChild.className === 'campaign__error') ) {
+        campaign.insertAdjacentHTML('afterbegin', '<p class="campaign__error">Sorry, there was an error submitting your contribution. Please, try again.</p>')
+      }
+    }
+
+    return {
+      init: init
+    };
+});
