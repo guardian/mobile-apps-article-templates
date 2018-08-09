a6d0fdbc33022fd6c47e996f450c30430e988052
Jamie Webb <webb04@users.noreply.github.com>
2018-08-09 16:48:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,3 @@ yarn test
 echo "//registry.npmjs.org/:_authToken=$NODE_TOKEN" >> ~/.npmrc
 npm --no-git-tag-version version 1.0.${BUILD_NUMBER}
 npm publish --access public
-
-# Update repos
-# ./update.sh ios-live 1.0.${BUILD_NUMBER} ${BRANCH_NAME}
-# ./update.sh android-news-app 1.0.${BUILD_NUMBER} ${BRANCH_NAME}
