bb2e121198f6e027e4b41a43152b338f535df1bc
Jamie Webb <jamie.webb@guardian.co.uk>
2018-10-09 17:52:16 +0100

    Handle cases where rich links precede atoms

---
+++ ArticleTemplates/assets/js/modules/rich-links.js
@@ -0,0 +1,23 @@
+define(function() {
+    'use strict';
+
+    function hasClass(element, className) {
+        return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+    }
+
+    function init() {
+        var richLinks = document.getElementsByClassName('element-rich-link');
+
+        for (var i = 0; i < richLinks.length; i++) {
+            var currentLink = richLinks[i];
+            if (hasClass(currentLink.nextElementSibling, 'element-atom')) {
+                currentLink.style.width = "100%";
+                currentLink.nextElementSibling.style.clear = "both";
+            }
+        }
+    }
+
+    return {
+        init: init
+    };
+});
