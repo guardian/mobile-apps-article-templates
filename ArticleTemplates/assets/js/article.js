87445ed3a2f4b4e72acd927a261115e5116efb67
Jamie Webb <jamie.webb@guardian.co.uk>
2018-12-14 11:30:00 +0000

    keep campaign module

--- ArticleTemplates/assets/js/article.js
+++ ArticleTemplates/assets/js/article.js
@@ -1,11 +1,13 @@
 import { init as commonInit } from 'bootstraps/common';
 import { init as articleInit } from 'bootstraps/article';
 import { init as atomsInit } from 'bootstraps/atoms';
+import { init as campaignInit } from 'bootstraps/campaign';
 
 const init = () => {
     commonInit();
     articleInit();
     atomsInit();
+    campaignInit();
 };
 
 export { init };
