f8f1eceb25b0e3481bfa857b2734114a81acf546
Jamie Webb <jamie.webb@guardian.co.uk>
2019-01-02 11:20:32 +0000

    merge master

--- ArticleTemplates/assets/js/modules/creativeInjector.js
+++ ArticleTemplates/assets/js/modules/creativeInjector.js
@@ -57,13 +57,36 @@ function injectHTML(html, id, type) {
     creativeContainer.classList.add(`${type}-creative-container`);
     creativeContainer.innerHTML = html;
 
-    if (type === 'inline-article') {
+    if (type === 'inline' && id === 'midarticle') {
+        injectInlineCreativeIfLength(creativeContainer, 10);
+    } else if (type === 'inline-article') {
         injectInlineCreative(creativeContainer);
     } else {
         injectEpicCreative(creativeContainer);
     }
 
     addEventListenerScroll(creativeContainer, id);
+}
+
+function injectInlineCreativeIfLength(creativeContainer, minParagraphs) {
+    let i;
+    let prose = document.querySelector('.article__body > div.prose');
+    let paragraphs = prose.querySelectorAll('p:nth-child(n+6)');
+
+     // Don't show creative if less than 10 paragraphs
+    if (document.querySelectorAll('.article__body > div.prose > p').length < minParagraphs) {
+        return;
+    }
+
+     // loop through paragraphs from 4th paragraph
+    // insert creativeContainer if paragraph is followed by a p or h1 elem
+    for (i = 0; i < paragraphs.length; i++) {
+        if (paragraphs[i].nextElementSibling &&
+            (paragraphs[i].nextElementSibling.tagName === 'P' || paragraphs[i].nextElementSibling.tagName === 'H1')) {
+            paragraphs[i].nextElementSibling.parentNode.insertBefore(creativeContainer, paragraphs[i].nextElementSibling);
+            break;
+        }
+    }
 }
 
 function injectInlineCreative(creativeContainer) {
