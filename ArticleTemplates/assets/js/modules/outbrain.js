64ed381b95f0e4aadbf5fb4627fd44edb74bba78
Jamie Webb <jamie.webb@guardian.co.uk>
2019-01-25 14:03:09 +0000

    Outbrain commercial changes

--- ArticleTemplates/assets/js/modules/outbrain.js
+++ ArticleTemplates/assets/js/modules/outbrain.js
@@ -1,11 +1,8 @@
-function getSection () {
-    const sections = ['politics', 'world', 'business', 'commentisfree'];
-    const section = GU.opts.section;
-
-    return section.toLowerCase().match('news') || sections.indexOf(section.toLowerCase()) > 0 ? 'sections' : 'all';
+function isNonCompliant() {
+    return GU.opts.contentType === 'liveblog';
 }
 
-function isNonCompliant() {
+function hasEpic() {
     return GU.opts.hasEpic;
 }
 
@@ -13,75 +10,28 @@ function init() {
     let contentUrl;
     const isTablet = GU.opts.adsConfig === 'tablet' ? true : false;
     const outbrainContainer = document.getElementsByClassName('container__outbrain')[0];
-    const outbrainImage = document.getElementsByClassName('outbrainImage')[0];
-    const outbrainText = document.getElementsByClassName('outbrainText')[0];
-    const section = getSection();
+
+    const outbrainWidget = document.getElementsByClassName('OUTBRAIN')[0];
+
     let scriptElement;
-    let widgetCodeText;
     let widgetConfig;
-    let widgetCodeImage;
 
     if (!outbrainContainer) {
         return;
     }
 
-    contentUrl = outbrainImage.dataset.src;
+    contentUrl = outbrainWidget.dataset.src;
 
     if (contentUrl) {
         outbrainContainer.style.display = 'block';
 
         if (isTablet) {
-            if (isNonCompliant()) {
-                widgetConfig = {
-                    image: {
-                        sections: 'AR_20',
-                        all: 'AR_20'
-                    },
-                    text: {
-                        sections: 'AR_20',
-                        all: 'AR_20'
-                    }
-                };
-            } else {
-                widgetConfig = {
-                    image: {
-                        sections: 'AR_12',
-                        all: 'AR_12'
-                    },
-                    text: {
-                        sections: 'AR_12',
-                        all: 'AR_12'
-                    }
-                };
-            }
-
-            outbrainText.style.display = 'block';
-            widgetCodeText = widgetConfig.text[section];
-            outbrainText.dataset.widgetId = widgetCodeText;
+            widgetConfig = isNonCompliant() ? 'AR_20' : (hasEpic() ? 'AR_24' : 'AR_12');
         } else {
-            if (isNonCompliant()) {
-                widgetConfig = {
-                    image: {
-                        sections: 'AR_29',
-                        all: 'AR_29'
-                    }
-                };
-            } else {
-                widgetConfig = {
-                    image: {
-                        sections: 'AR_16',
-                        all: 'AR_16'
-                    }
-                };
-            }
-
-            if (outbrainContainer.childElementCount > 0 && outbrainText) {
-                outbrainContainer.removeChild(outbrainText);
-            }
+            widgetConfig = isNonCompliant() ? 'AR_29' : (hasEpic() ? 'AR_22' : 'AR_16');
         }
 
-        widgetCodeImage = widgetConfig.image[section];
-        outbrainImage.dataset.widgetId = widgetCodeImage;
+        outbrainWidget.dataset.widgetId = widgetConfig;
 
         scriptElement = document.createElement('script');
         scriptElement.id = 'outbrain-widget';
