dbd010321a376b4b9fd8c0d8f8c96ebbef5d9588
Jamie Webb <jamie.webb@guardian.co.uk>
2019-02-21 17:33:11 +0000

    Pinch and zoom galleries

--- ArticleTemplates/assets/js/bootstraps/gallery.js
+++ ArticleTemplates/assets/js/bootstraps/gallery.js
@@ -1,118 +1,100 @@
-import { debounce } from 'modules/util';
-
-let galleryProps;
-let galleryRows;
-
-const PROPS = {
-        0 :{
-            imagesPerRow: 3
-        },
-        650: {
-            imagesPerRow: 4
-        },
-        1200: {
-            imagesPerRow: 5
-        }
-    };
+import Hammer from 'hammerjs';
 
 function init() {
-    buildGallery();
-    window.addEventListener('resize', debounce(buildGallery, 100));
-}
+    var galleryImages = document.querySelectorAll('.touch-gallery__images');
+    galleryImages.forEach(function(galleryImage) {
+        var hammerSettings = (GU.opts.platform === 'ios' ? { touchAction: 'auto' } : {})
+        var mc = new Hammer.Manager(galleryImage, hammerSettings);
+        var pinch = new Hammer.Pinch();
 
-function buildGallery() {
-    setGalleryProps();
-    addImagesToRows();
-    addFlexClass();
-}
+        mc.add(pinch);
 
-function setGalleryProps() {
-    let width;
-
-    for (width in PROPS) {
-        if(PROPS.hasOwnProperty(width)) {
-            if (window.innerWidth > parseInt(width, 10)) {
-                galleryProps = PROPS[width];
-            } else {
-                break;
+        mc.on('pinch', function(ev) {
+            if (GU.opts.platform === 'ios') {
+                ev.preventDefault();
             }
+
+            var desiredScale = ev.scale;
+            var pinchCentre = getPinchCentre(galleryImage);
+
+            galleryImage.style.transformOrigin = pinchCentre.x+'% '+pinchCentre.y+'%';
+            galleryImage.style.transform = 'scale('+desiredScale+')';
+        });
+
+        mc.on('pinchstart', function(ev) {
+            galleryImage.classList.add('touch-gallery__images--pinch');
+            lockArticleSwipe(true);
+            savePinchCentre(galleryImage, ev);
+            loadHighRes(galleryImage);
+        });
+
+        mc.on('pinchend', function() {
+            lockArticleSwipe(false);
+            bounceToInitialPosition(galleryImage);
+        });
+
+    });
+
+    function loadHighRes(el) {
+        if (!el.classList.contains('high-res-setup')) {
+            el.classList.add('high-res-setup');
+            var highRes = el.querySelector('.touch-gallery__images__high-res');
+            highRes.setAttribute('src', highRes.dataset.src);
+            highRes.addEventListener('load', function() {
+                el.classList.add('high-res-loaded');
+            });
         }
     }
-}
 
-function addImagesToRows() {
-    let i;
-    const images = document.body.getElementsByClassName('gallery__image-container');
-
-    const rows = [
-        []
-    ];
-
-    // loop through images and add to rows
-    for (i = images.length - 1; i >= 0; i--) {
-        // if current row is full create new row
-        if (rows[0].length === galleryProps.imagesPerRow) {
-            rows.unshift([]);
+    function lockArticleSwipe(toggle) {
+        var isAndroidApp = (window.location.origin === "file://" && /(android)/i.test(navigator.userAgent) ) ? true : false;
+        if (isAndroidApp) {
+            window.GuardianJSInterface.registerRelatedCardsTouch(toggle);
         }
-        // add next image to front of new row
-        rows[0].unshift(images[i]);
     }
 
-    // if first row doesn't have enough images call moveImagesToRows
-    if (rows.length > 1 && rows[0].length < (galleryProps.imagesPerRow - 1)) {
-        moveImagesToRows(rows);
+    function getPinchCentre(el) {
+        var pinchX = el.dataset.pinchCentreX;
+        var pinchY = el.dataset.pinchCentreY;
+        return {x: pinchX, y: pinchY};
     }
 
-    galleryRows = rows;
-}
+    function savePinchCentre(el, ev) {
+        var elBounds = el.getBoundingClientRect();
+        var pinchCentreX = calcPinchCentre(elBounds, ev.center, 'x', 'width');
+        var pinchCentreY = calcPinchCentre(elBounds, ev.center, 'y', 'height');
 
-function moveImagesToRows(rows) {
-    let i;
-    let j;
-    let offset;
+        el.dataset.pinchCentreX = pinchCentreX;
+        el.dataset.pinchCentreY = pinchCentreY;
+    }
 
-    for (i = 0; i < rows.length; i++) {
-        if (i === rows.length - 1) {
-            while (rows[i].length < rows[i - 1].length) {
-                rows[i].push(rows[i - 1].pop());
-            }
+    function calcPinchCentre(elBounds, pinchCentre, axis, dimension) {
+        var elStart = elBounds[axis];
+        var elEnd = elBounds[dimension];
+        var pinch = pinchCentre[axis];
+
+        var pinchRel = (pinch-elStart)/(elEnd);
+        if (pinchRel > 0.75) {
+            return 100
+        } else if (pinchRel < 0.25) {
+            return 0
         } else {
-            offset = (galleryProps.imagesPerRow - 1) - rows[i].length;
-
-            if (offset > 0) {
-                for (j = 0; j < offset; j++) {
-                    if (rows[i + 1]) {
-                        rows[i].push(rows[i + 1].shift());
-                    }
-                }
-            }
+            return Math.round(pinchRel*100);
         }
     }
-}
 
-function addFlexClass() {
-    let i;
-    let j;
-
-    // for each row
-    for (i = 0; i < galleryRows.length; i++) {
-        // for each image in row
-        for (j = 0; j < galleryRows[i].length; j++) {
-            // remove flex class
-            removeFlexClass(galleryRows[i][j]);
-            // add new flex class
-            galleryRows[i][j].classList.add(`gallery__image-container--flex${galleryRows[i].length}`);
-        }
-    }
-}
-
-function removeFlexClass(imageElem) {
-    let i;
-
-    // if imageElem has class name with "flex" in it then remove class
-    for (i = 0; i < imageElem.classList.length; i++) {
-        if (imageElem.classList[i].includes('flex')) {
-            imageElem.classList.remove(imageElem.classList[i]);
+    function bounceToInitialPosition(el) {
+        var currentScale = (el.getBoundingClientRect().width/el.offsetWidth);
+        var newScale = currentScale + (1-currentScale)/5;
+        if (newScale > 0.99 && newScale < 1.01) {
+            lockArticleSwipe(false);
+            el.style.transform = 'scale(1)';
+            el.classList.remove('touch-gallery__images--pinch');
+        } else {
+            el.style.transform = 'scale('+newScale+')';
+            requestAnimationFrame(function() {
+                bounceToInitialPosition(el);
+            });
         }
     }
 }
