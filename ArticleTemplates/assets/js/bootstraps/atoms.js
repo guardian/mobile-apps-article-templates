1549a218575401070d894c28b6f1d7edfd4c1100
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2019-02-26 10:53:31 +0000

    Add bootstrap for charts

--- ArticleTemplates/assets/js/bootstraps/atoms.js
+++ ArticleTemplates/assets/js/bootstraps/atoms.js
@@ -11,6 +11,47 @@ function init() {
             return;
         }
         bootAtomType(t, atomTypes[t]);
+        if (t === 'chart') {
+            initCharts();
+        }
+    });
+}
+
+function initCharts() {
+    var iframes = Array.prototype.slice.call(document.querySelectorAll('.atom--chart > .atom__iframe'));
+    window.addEventListener('message', function (event) {
+        var message;
+        var iframe = iframes.reduce(function (winner, candidate) {
+            if (winner) {
+                return winner;
+            }
+
+            try {
+                return candidate.name === event.source.name ? candidate : null;
+            } catch (e) {
+                return null;
+            }
+        }, null);
+        if (iframe) {
+            try {
+                message = JSON.parse(event.data);
+                switch (message.type) {
+                case 'set-height':
+                    iframe.height = message.value;
+                    break;
+                default:
+                }
+                // eslint-disable-next-line no-empty
+            } catch (e) {}
+        }
+    });
+
+    iframes.forEach(function (iframe) {
+        const src = (iframe.getAttribute('srcdoc') || '')
+            .replace(/<gu-script>/g, '<script>')
+            // eslint-disable-next-line no-useless-concat
+            .replace(/<\/gu-script>/g, '<' + '/script>');
+        iframe.setAttribute('srcdoc', src);
     });
 }
 
