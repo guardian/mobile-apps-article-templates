6c73899235bfbc504a60e262ff6396338b949a27
Jamie Webb <jamie.webb@guardian.co.uk>
2019-10-31 11:21:02 +0000

    replace chrome api

--- ArticleTemplates/assets/js/modules/util.js
+++ ArticleTemplates/assets/js/modules/util.js
@@ -133,15 +133,15 @@ function getAndroidVersion() {
     return match ? match[1] : undefined;
 }
 
-const reportScrollFinish = event =>
-    signalDevice(`scrollFinished/${event.detail.anchor}`);
+const reportScrollFinish = event => {
+    signalDevice(`scrollFinished/${event.detail.anchor.id}`);
+    document.removeEventListener('scrollStop', reportScrollFinish, false);
+}
 
 function scrollToElement(element) {
     const scroll = new SmoothScroll();
     scroll.animateScroll(element, { speed: 1500 });
-    if (!getEventListeners(document).scrollStop) {
-        document.addEventListener('scrollStop', reportScrollFinish, false);
-    }
+    document.addEventListener('scrollStop', reportScrollFinish, false);
 }
 
 function isAdvertising() {
