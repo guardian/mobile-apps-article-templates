aba7475292dfa74c64ffd4d7ba5e4aac5f76d7f6
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-04-06 15:11:51 +0200

    adds livereload

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = (env, argv) => {
   const config = {
@@ -17,102 +18,104 @@ module.exports = (env, argv) => {
     },
     entry: {
       boot: './ArticleTemplates/assets/js/boot.js',
-        'style-sync': './ArticleTemplates/assets/scss/style-sync.scss',
-        'style-async': './ArticleTemplates/assets/scss/style-async.scss',
-        'fonts-ios': './ArticleTemplates/assets/scss/fonts-ios.scss',
-        'fonts-android': './ArticleTemplates/assets/scss/fonts-android.scss',
-        'interactive': './ArticleTemplates/assets/scss/interactive.scss',
+      'style-sync': './ArticleTemplates/assets/scss/style-sync.scss',
+      'style-async': './ArticleTemplates/assets/scss/style-async.scss',
+      'fonts-ios': './ArticleTemplates/assets/scss/fonts-ios.scss',
+      'fonts-android': './ArticleTemplates/assets/scss/fonts-android.scss',
+      'interactive': './ArticleTemplates/assets/scss/interactive.scss',
+    },
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      path: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader"
+        ],
       },
-      output: {
-        filename: '[name].js',
-        chunkFilename: '[name].js',
-        path: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: ['babel-loader']
       },
-      module: {
-        rules: [{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [
-            "babel-loader"
-          ],
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          path.resolve(__dirname, "ArticleTemplates/assets/js/bootstraps/"),
+          path.resolve(__dirname, "ArticleTemplates/assets/js/modules/")
+        ],
+        use: ['eslint-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
         },
         {
-          test: /\.js$/,
-          exclude: [/node_modules/],
-          use: ['babel-loader']
+          loader: "css-loader",
+          options: {
+            url: false,
+            minimize: true,
+            sourceMap: argv.mode !== 'production',
+          }
         },
         {
-          test: /\.js$/,
-          exclude: [
-            /node_modules/,
-            path.resolve(__dirname, "ArticleTemplates/assets/js/bootstraps/"),
-            path.resolve(__dirname, "ArticleTemplates/assets/js/modules/")
-          ],
-          use: ['eslint-loader']
-        },
-        {
-          test: /\.scss$/,
-          use: [{
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-              options: {
-                url: false,
-                minimize: true,
-                sourceMap: argv.mode !== 'production',
-              }
-          },
-          {
-            loader: 'sass-loader',
-              options: {
-                sourceMap: argv.mode !== 'production'
-              }
-          }]
+          loader: 'sass-loader',
+          options: {
+            sourceMap: argv.mode !== 'production'
+          }
         }]
+      }]
+    },
+    resolve: {
+      modules: [
+        path.resolve(__dirname, 'ArticleTemplates/assets/js'),
+        "node_modules"
+      ]
+    },
+    plugins: [
+      new LiveReloadPlugin(),
+
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+      new webpack.DefinePlugin({
+        BUILD_NUMBER: process.env.BUILD_NUMBER || 0
+      }),
+      new CopyWebpackPlugin([{
+        from: './node_modules/curl/dist/curl',
+        to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
       },
-      resolve: {
-        modules: [
-          path.resolve(__dirname, 'ArticleTemplates/assets/js'),
-            "node_modules"
-        ]
+      {
+        from: 'ArticleTemplates/assets/js/modules/mobile-range-slider.js',
+        to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
       },
-      plugins: [
-        new webpack.optimize.LimitChunkCountPlugin({
-          maxChunks: 1,
-        }),
-        new webpack.DefinePlugin({
-          BUILD_NUMBER: process.env.BUILD_NUMBER || 0
-        }),
-        new CopyWebpackPlugin([{
-          from: './node_modules/curl/dist/curl',
-          to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
-        },
-        {
-          from: 'ArticleTemplates/assets/js/modules/mobile-range-slider.js',
-          to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
-        },
-        {
-          from: './node_modules/intersection-observer/intersection-observer.js',
-          to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
-        },
-        {
-          from: './node_modules/classlist-polyfill/src/index.js',
-          to: path.resolve(__dirname, 'ArticleTemplates/assets/build/classlist-polyfill'),
-        },
-        {
-          from: './node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js',
-          to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
-        }]),
-        new MiniCssExtractPlugin({
-          filename: "../css/[name].css",
-          chunkFilename: "../css/[name].css",
-        })],
-    };
-    
-    if (argv.mode !== 'production') {
-      config.devtool = 'source-map';
-    }
-    
-    return config;
+      {
+        from: './node_modules/intersection-observer/intersection-observer.js',
+        to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
+      },
+      {
+        from: './node_modules/classlist-polyfill/src/index.js',
+        to: path.resolve(__dirname, 'ArticleTemplates/assets/build/classlist-polyfill'),
+      },
+      {
+        from: './node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js',
+        to: path.resolve(__dirname, 'ArticleTemplates/assets/build'),
+      }]),
+      new MiniCssExtractPlugin({
+        filename: "../css/[name].css",
+        chunkFilename: "../css/[name].css",
+      })],
+  };
+
+  if (argv.mode !== 'production') {
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
