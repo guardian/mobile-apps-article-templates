1053c2252df10ed962554a5630b1a2fc989dbf60
Jamie Webb <jamie.webb@guardian.co.uk>
2020-10-30 13:57:27 +0000

    detect when premium taster is in viewport

--- ArticleTemplates/assets/js/modules/premiumTaster.js
+++ ArticleTemplates/assets/js/modules/premiumTaster.js
@@ -1,33 +1,37 @@
+import {
+    debounce,
+    signalDevice,
+    isElementPartiallyInViewport,
+} from 'modules/util';
+
 function init() {
     try {
         const showAdFreeTaster = window.GU.opts.showAdFreeTaster;
-        setInterval(() => {
-            console.log("showAdFreeTaster-------------------")
-            console.log(showAdFreeTaster);
-        }, 1000)
         if (!showAdFreeTaster) {
             return;
         }
 
         const afterParagraphs = 3;
         const placeholder = document.createElement('div');
-        const adFreeTasterSibling = document.querySelector(`.article__body > div.prose > p:nth-of-type(${afterParagraphs}) ~ p + p`);
+        const adFreeTasterSibling = document.querySelector(`.article__body > div.prose > p:nth-of-type(${afterParagraphs - 1}) ~ p + p`);
 
         if (!(adFreeTasterSibling && adFreeTasterSibling.parentNode)) {
             // Not enough paragraphs on page to add adFree taster
             return;
         }
-
-        setInterval(() => {
-            console.log("adFreeTasterSibling-------------------")
-            console.log(adFreeTasterSibling);
-        }, 1000)
         
         placeholder.classList.add('ad-free-taster');
         adFreeTasterSibling.parentNode.insertBefore(placeholder, adFreeTasterSibling);
-
+        window.addEventListener('scroll', debounce(isAdFreePremiumTasterInView.bind(null, placeholder), 200));
     } catch (e) {
+        console.error(e);
+    }
+}
 
+function isAdFreePremiumTasterInView(AdFreePremiumTaster) {
+    if (isElementPartiallyInViewport(AdFreePremiumTaster)) {
+        console.log("taster in viewport, report to native layers");
+        signalDevice('trackAction/AdFreePremiumTaster:seen');
     }
 }
 
