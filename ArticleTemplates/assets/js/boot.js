1d0a7cb074051c3b41ab4c9c63b55cc79b1373a8
George B <705427+georgeblahblah@users.noreply.github.com>
2022-01-07 11:36:23 +0000

    Move email embed resize listener to common bootstraps

--- ArticleTemplates/assets/js/boot.js
+++ ArticleTemplates/assets/js/boot.js
@@ -158,45 +158,6 @@ const init = opts => {
                 common.init();
             });
         }
-
-        const listenForEmailEmbedIFrameResize = () => {
-            const allowedOrigins = ['https://www.theguardian.com'];
-
-            const allIframes = [].slice.call(
-                document.querySelectorAll('.email-sub__iframe')
-            );
-            window.addEventListener('message', (event) => {
-                if (!allowedOrigins.includes(event.origin)) return;
-
-                const iframes = allIframes.filter((i) => {
-                    try {
-                        return i.contentWindow === event.source;
-                    } catch (e) {
-                        return false;
-                    }
-                });
-                if (iframes.length !== 0) {
-                    try {
-                        const message = JSON.parse(event.data);
-                        switch (message.type) {
-                        case 'set-height': {
-                            const value = parseInt(message.value);
-                            if (!Number.isInteger(value)) return;
-
-                            iframes.forEach((iframe) => {
-                                iframe.height = `${value}`;
-                            });
-                            break;
-                        }
-                        default:
-                        }
-                        // eslint-disable-next-line no-empty
-                    } catch (e) {}
-                }
-            });
-        };
-
-        listenForEmailEmbedIFrameResize();
     };
 
     if (test) {
--- ArticleTemplates/assets/js/bootstraps/common.js
+++ ArticleTemplates/assets/js/bootstraps/common.js
@@ -286,6 +286,7 @@ function loadEmbeds() {
 
     fixVineWidth();
     fixEmbedHeights();
+    listenForEmailEmbedIFrameResize();
 
     for (i = 0; i < fenceElems.length; i++) {
         render(fenceElems[i]);
@@ -321,6 +322,43 @@ function fixEmbedHeights() {
         iframe.setAttribute('height', '50px');
         iframe.style.marginTop = '12px';
     }
+}
+
+function listenForEmailEmbedIFrameResize() {
+    const allowedOrigins = ['https://www.theguardian.com'];
+
+    const allIframes = [].slice.call(
+        document.querySelectorAll('.email-sub__iframe')
+    );
+    window.addEventListener('message', (event) => {
+        if (!allowedOrigins.includes(event.origin)) return;
+
+        const iframes = allIframes.filter((i) => {
+            try {
+                return i.contentWindow === event.source;
+            } catch (e) {
+                return false;
+            }
+        });
+        if (iframes.length !== 0) {
+            try {
+                const message = JSON.parse(event.data);
+                switch (message.type) {
+                case 'set-height': {
+                    const value = parseInt(message.value);
+                    if (!Number.isInteger(value)) return;
+
+                    iframes.forEach((iframe) => {
+                        iframe.height = `${value}`;
+                    });
+                    break;
+                }
+                default:
+                }
+                // eslint-disable-next-line no-empty
+            } catch (e) {}
+        }
+    });
 }
 
 function loadInteractives(force) {
