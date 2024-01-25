d9b1a5e3d7c10ed3ff43160cba78ff08ec155fa3
Lindsey Dew <1202622+lindseydew@users.noreply.github.com>
2024-01-25 14:50:51 +0000

    Display buttons as inline-block

--- ArticleTemplates/assets/js/modules/myGuardianFollowInit.js
+++ ArticleTemplates/assets/js/modules/myGuardianFollowInit.js
@@ -2,7 +2,7 @@ function init() {
     if (GU.opts.myGuardianEnabled) {
         const followButtons = document.querySelectorAll('.my-guardian-follow-tag');
         followButtons.forEach((followButton) => {
-            followButton.style.display = 'block';
+            followButton.style.display = 'inline-block';
         });
     }
 }
